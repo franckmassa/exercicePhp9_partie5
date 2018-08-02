b4def33507b4cc474a9d88ae966d44ee607155c1
massa <massafranck@gmail.com>
2018-08-02 16:10:31 +0200

    corrigé

--- index.php
+++ index.php
@@ -1,21 +1,20 @@
 <?php
 //Déclaration de la variable haut De France avec pour index le numéro des départements
 $hautDeFrance = array('60' => 'Oise', '02' => 'Aisne',
-'80' => 'Somme', '59' => 'Nord', '62' => 'Pas-De-Calais');
+    '80' => 'Somme', '59' => 'Nord', '62' => 'Pas-De-Calais');
 ?><!DOCTYPE html>
 <html lang="fr">
-<head>
-  <meta charset="utf-8" />
-  <title>Exercice9</title>
-</head>
-<body>
-  <p>
-    <?php
-    //Création d'une boucle pour récupérer toutes les données du tableau
-    foreach($hautDeFrance as $department){
-      echo $department . '<br/>';
-    }
-    ?>
-  </p>
-</body>
+    <head>
+        <meta charset="utf-8" />
+        <title>Exercice9</title>
+    </head>
+    <body>
+        <?php
+        //Création d'une boucle pour récupérer toutes les données du tableau
+        foreach ($hautDeFrance as $number => $department) {
+            ?>
+            <!--Affichage du tableau-->
+            <p><?php echo $number . ' ' . $department ?></p>
+<?php } ?>
+    </body>
 </html>
